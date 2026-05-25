62586279255de25b495544e9c2fe5556f46b076b
Arthur Abade <arthurnz.personal@gmail.com>
2026-05-25 16:49:09 -0300

    feat(services): implementação de CategoriaService

---
+++ estoque/src/main/java/com/arquiteturaweb/estoque/services/CategoriaService.java
@@ -0,0 +1,61 @@
+package com.arquiteturaweb.estoque.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.arquiteturaweb.estoque.entities.Categoria;
+import com.arquiteturaweb.estoque.repositories.CategoriaRepository;
+import com.arquiteturaweb.estoque.services.exceptions.DatabaseException;
+import com.arquiteturaweb.estoque.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public List<Categoria> findAll() {
+        return repository.findAll();
+    }
+
+    public Categoria findById(Long id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Categoria insert(Categoria obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Categoria update(Long id, Categoria obj) {
+        try {
+            Categoria entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Categoria entity, Categoria obj) {
+        entity.setNome(obj.getNome());
+    }
+
+}
