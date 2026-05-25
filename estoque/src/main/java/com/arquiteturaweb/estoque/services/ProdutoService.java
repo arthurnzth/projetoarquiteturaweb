b94adb077d0252250b5f839105d52cf98b8c936e
Arthur Abade <arthurnz.personal@gmail.com>
2026-05-25 17:03:55 -0300

    feat(ProdutoService): implementação de funções findById, insert, delete e update para Produto

--- estoque/src/main/java/com/arquiteturaweb/estoque/services/ProdutoService.java
+++ estoque/src/main/java/com/arquiteturaweb/estoque/services/ProdutoService.java
@@ -4,24 +4,62 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.arquiteturaweb.estoque.entities.Produto;
 import com.arquiteturaweb.estoque.repositories.ProdutoRepository;
+import com.arquiteturaweb.estoque.services.exceptions.DatabaseException;
+import com.arquiteturaweb.estoque.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProdutoService {
 
     @Autowired
-    private ProdutoRepository repositorio;
+    private ProdutoRepository repository;
 
     public List<Produto> findAll() {
-        return repositorio.findAll();
+        return repository.findAll();
     }
 
     public Produto findById(Long id) {
-        Optional<Produto> obj = repositorio.findById(id);
-        return obj.get();
+        Optional<Produto> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Produto insert(Produto obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Produto update(Long id, Produto obj) {
+        try {
+            Produto entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Produto entity, Produto obj) {
+        entity.setNome(obj.getNome());
+        entity.setDescricao(obj.getDescricao());
+        entity.setPreco(obj.getPreco());
+        entity.getCategorias().clear();
+        entity.getCategorias().addAll(obj.getCategorias());
     }
 
 }
