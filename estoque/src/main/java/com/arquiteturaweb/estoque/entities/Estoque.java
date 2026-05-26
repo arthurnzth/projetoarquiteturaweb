1c077b2b971243a1fed033f754e995bc5ca6d16e
Arthur Abade <arthurnz.personal@gmail.com>
2026-05-26 17:53:41 -0300

    feat(entities): criação da entidade Estoque

---
+++ estoque/src/main/java/com/arquiteturaweb/estoque/entities/Estoque.java
@@ -0,0 +1,90 @@
+package com.arquiteturaweb.estoque.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_estoque")
+public class Estoque implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long quantidade;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    public Estoque() {
+
+    }
+
+    public Estoque(Long id, Produto produto) {
+        this.id = id;
+        this.quantidade = 0L;
+        this.produto = produto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Estoque other = (Estoque) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
