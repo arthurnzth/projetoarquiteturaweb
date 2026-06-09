0fd88279d38258be5a2f51f63c00906f641ce08a
Arthur Abade <arthurnz.personal@gmail.com>
2026-06-09 10:43:47 -0300

    style(test): adequação aos padrões de dev e limpeza de imports não usados

--- estoque/src/test/java/com/arquiteturaweb/estoque/services/CategoriaServiceTest.java
+++ estoque/src/test/java/com/arquiteturaweb/estoque/services/CategoriaServiceTest.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
