966ed64c62d6ddf9eb3eb9d4455353df4027f22b
Arthur Abade <arthurnz.personal@gmail.com>
2026-06-09 12:20:26 -0300

    feat(ItemPedidoPK): criação da entidade de PK para relação de Produto e Pedido

---
+++ estoque/src/main/java/com/arquiteturaweb/estoque/entities/pk/ItemPedidoPK.java
@@ -0,0 +1,9 @@
+package com.arquiteturaweb.estoque.entities.pk;
+
+import java.io.Serializable;
+
+public class ItemPedidoPK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+}
