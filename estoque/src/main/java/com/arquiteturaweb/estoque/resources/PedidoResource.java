e031cc7475424fb20d05c7952d3731d2c39bd31d
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-06-09 00:17:34 -0300

    feat(PedidoResource): Implementação dos métodos PUT e DELETE.

--- estoque/src/main/java/com/arquiteturaweb/estoque/resources/PedidoResource.java
+++ estoque/src/main/java/com/arquiteturaweb/estoque/resources/PedidoResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +54,20 @@ public class PedidoResource {
         return ResponseEntity.created(uri).body(responseObj);
     }
 
+
+    //MÉTODO PUT
+    // update
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PedidoResponseDTO> update(@PathVariable Long id, @RequestBody PedidoRequestDTO obj) {
+        PedidoResponseDTO responseObj = service.update(id, obj);
+        return ResponseEntity.ok().body(responseObj);
+    }
+
+    //MÉTODO DELETE
+     // delete
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
