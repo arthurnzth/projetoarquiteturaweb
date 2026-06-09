bf8acea79fa9dac68eee3f30e022d7e821c90d39
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-06-08 23:13:16 -0300

    feat(PedidoRequestDTO): Criação de classe, atributos, construtores, getters e setters.

---
+++ estoque/src/main/java/com/arquiteturaweb/estoque/entities/dto/pedido/PedidoRequestDTO.java
@@ -0,0 +1,49 @@
+package com.arquiteturaweb.estoque.entities.dto.pedido;
+
+import java.io.Serializable;
+
+public class PedidoRequestDTO implements Serializable{
+    
+    private static final Long serialVersionUID = 1L;
+
+    private Double valorTotal;
+    private Long fornecedorId;
+    private Long movimentacaoId;
+
+    public PedidoRequestDTO(){
+
+    }
+
+    public PedidoRequestDTO(Double valorTotal, Long fornecedorId, Long movimentacaoId){
+        this.valorTotal = valorTotal;
+        this.fornecedorId = fornecedorId;
+        this.movimentacaoId = movimentacaoId;
+    }
+
+
+    public Double getValorTotalRequestPedido() {
+        return valorTotal;
+    }
+
+    public void setValorTotalRequestPedido(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Long getFornecedorIdPedidoRequest() {
+        return fornecedorId;
+    }
+
+    public void setFornecedorIdPedidoRequest(Long fornecedorId) {
+        this.fornecedorId = fornecedorId;
+    }
+
+    public Long getMovimentacaoIdPedidoRequest() {
+        return movimentacaoId;
+    }
+
+    public void setMovimentacaoIdPedidoRequest(Long movimentacaoId) {
+        this.movimentacaoId = movimentacaoId;
+    }
+
+
+}
