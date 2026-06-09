e65c41aeb90c19e7df418056a078626286d29e80
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-06-09 00:01:51 -0300

    feat(PedidoService): Implementação completa do método PUT.

--- estoque/src/main/java/com/arquiteturaweb/estoque/services/PedidoService.java
+++ estoque/src/main/java/com/arquiteturaweb/estoque/services/PedidoService.java
@@ -10,14 +10,19 @@ import org.springframework.stereotype.Service;
 import com.arquiteturaweb.estoque.entities.Categoria;
 import com.arquiteturaweb.estoque.entities.Fornecedor;
 import com.arquiteturaweb.estoque.entities.Pedido;
+import com.arquiteturaweb.estoque.entities.dto.fornecedor.FornecedorRequestDTO;
+import com.arquiteturaweb.estoque.entities.dto.fornecedor.FornecedorResponseDTO;
 import com.arquiteturaweb.estoque.entities.dto.pedido.PedidoRequestDTO;
 import com.arquiteturaweb.estoque.entities.dto.pedido.PedidoResponseDTO;
 import com.arquiteturaweb.estoque.entities.Produto;
+import com.arquiteturaweb.estoque.entities.dto.produto.ProdutoRequestDTO;
 import com.arquiteturaweb.estoque.entities.dto.produto.ProdutoResponseDTO;
 import com.arquiteturaweb.estoque.repositories.FornecedorRepository;
 import com.arquiteturaweb.estoque.repositories.PedidoRepository;
 import com.arquiteturaweb.estoque.services.exceptions.DatabaseException;
 import com.arquiteturaweb.estoque.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PedidoService {
@@ -70,6 +75,30 @@ public class PedidoService {
             }
         }catch (InterruptedException e) {
             throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    //MÉTODO PUT
+    public PedidoResponseDTO update(Long id, PedidoRequestDTO requestData){
+        try {
+            Pedido entity = pedidoRepositorio.getReferenceById(id);
+            updateData(entity, requestData);
+            return PedidoResponseDTO.converterPedido(pedidoRepositorio.save(entity));
+        
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Pedido entity, PedidoRequestDTO obj) {
+
+        try {
+            entity.setValorTotalPedido(obj.getValorTotalRequestPedido());
+            entity.setFornecedorPedido(fornecedorRepository.getReferenceById(obj.getFornecedorIdPedidoRequest()));
+            entity.setMovimentacaoPedido(movimentacaoRepository.getReferenceById(obj.getFornecedorIdPedidoRequest()));
+
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(obj.getFornecedorIdPedidoRequest());
         }
     }
 
